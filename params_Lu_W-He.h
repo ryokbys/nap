ef8505548da040cd1a06a4d1768604bb16f36adf
Ryo KOBAYASHI <ryo.kbys@gmail.com>
2013-07-09 12:28:12 +0900

    QMCL correction added to params_Lu_W-He.h.

--- params_Lu_W-He.h
+++ params_Lu_W-He.h
@@ -4,6 +4,9 @@ c-----mass
 c
 c  cut off should be larger than R+D (=4.13158A+0.93018A =5.06176A)
 c
+c.....length scaling for hybrid QMCL calculation
+      real(8),parameter:: slen= 1d0 /3.165d0 *3.172d0
+      real(8),parameter:: aa2bs= aa2bohr *slen
 c.....W-W and W-He parameters
       real(8),parameter:: p_D0(1:2,1:2)=(/
      &     2.87454d0 *ev2hrt,
@@ -12,16 +15,16 @@ c.....W-W and W-He parameters
      &     0d0 *ev2hrt
      &     /)
       real(8),parameter:: p_r0(1:2,1:2)=(/
-     &     2.38631d0 *aa2bohr,
-     &     3.40623d0 *aa2bohr,
-     &     3.40623d0 *aa2bohr,
-     &     0d0 *aa2bohr
+     &     2.38631d0 *aa2bs,
+     &     3.40623d0 *aa2bs,
+     &     3.40623d0 *aa2bs,
+     &     0d0 *aa2bs
      &     /)
       real(8),parameter:: p_beta(1:2,1:2)=(/
-     &     1.33682d0  /aa2bohr,
-     &     0.602677d0 /aa2bohr,
-     &     0.602677d0 /aa2bohr,
-     &     0d0 /aa2bohr
+     &     1.33682d0  /aa2bs,
+     &     0.602677d0 /aa2bs,
+     &     0.602677d0 /aa2bs,
+     &     0d0 /aa2bs
      &     /)
       real(8),parameter:: p_S(1:2,1:2)=(/
      &     1.25348d0,
@@ -54,20 +57,20 @@ c.....W-W and W-He parameters
      &     0d0
      &     /)
       real(8),parameter:: p_R1(1:2,1:2)=(/
-     &     4.131580d0 *aa2bohr,
-     &     3.00d0 *aa2bohr,
-     &     3.00d0 *aa2bohr,
-     &     0d0 *aa2bohr
+     &     4.131580d0 *aa2bs,
+     &     3.00d0 *aa2bs,
+     &     3.00d0 *aa2bs,
+     &     0d0 *aa2bs
      &     /)
       real(8),parameter:: p_D1(1:2,1:2)=(/
-     &     0.930180d0 *aa2bohr,
-     &     0.35d0 *aa2bohr,
-     &     0.35d0 *aa2bohr,
-     &     0d0 *aa2bohr
+     &     0.930180d0 *aa2bs,
+     &     0.35d0 *aa2bs,
+     &     0.35d0 *aa2bs,
+     &     0d0 *aa2bs
      &     /)
 c.....He-He parameters
       real(8),parameter:: p_HeHe_rc    = p_R1(1,1) +p_D1(1,1)
-      real(8),parameter:: p_HeHe_rm    = 2.970d0 *aa2bohr
+      real(8),parameter:: p_HeHe_rm    = 2.970d0 *aa2bs
       real(8),parameter:: p_HeHe_A     = 1.9211529d+5
       real(8),parameter:: p_HeHe_alpha = 10.73520708d0
       real(8),parameter:: p_HeHe_c6    = 1.34920045d0
