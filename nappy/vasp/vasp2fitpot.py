a0746ad738fdddf2c82409a1257b7011810f92a7
Ryo KOBAYASHI <ryo.kbys@gmail.com>
2015-08-28 10:26:27 +0900

    vasp2fitpot.py added into nappy

---
+++ nappy/vasp/vasp2fitpot.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+"""
+Extract data necessary for *fitpot* program from vasprun.xml:
+total energy, atom positions, forces on atoms, and cell information.
+This uses *Vasprun* class in the *pymatgen* package.
+Following files will be written:
+
+* pos
+* erg.ref
+* frc.ref
+
+If the vasprun.xml includes MD trajectory data, directories
+with 5-digit name are created and above three files are
+put in those directories.
+
+Usage:
+
+  $ python vasp2fitpot.py vasprun.xml
+
+"""
+
+__author__    = "Ryo KOBAYASHI"
+__email__     = "ryo.kbys@gmail.com"
+__copyright__ = "Copyright 2015, Ryo KOBAYASHI"
+__license__   = "MIT"
+__version__   = "0.1"
+
+_infname   = "vasprun.xml"
+_usage     = "%prog <vasprun.xml>"
+_fname_pos = "pos"
+_fname_erg = "erg.ref"
+_fname_frc = "frc.ref"
+
+import os,sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
+
+import optparse
+import numpy as np
+from pymatgen.io.vaspio.vasp_output import Vasprun
+from AtomSystem import AtomSystem
+from Atom import Atom
+
+
+
+def structure2aSys(structure):
+    """
+    Converts Structure object of pymatgen to AtomSystem object in nap.
+
+    Args:
+        structure (Structure): pymatgen Structure object to be converted
+        to AtomSystem object..
+
+    Returns:
+        aSys (AtomSystem): 
+    """
+    lattice= structure.lattice
+    alc= lattice.a
+    a1= np.array(lattice.matrix[0])
+    a2= np.array(lattice.matrix[1])
+    a3= np.array(lattice.matrix[2])
+    #... rescale a? vectors
+    a1= a1/alc
+    a2= a2/alc
+    a3= a3/alc
+    aSys= AtomSystem()
+    aSys.set_lattice(alc,a1,a2,a3)
+    for ia in range(structure.num_sites):
+        ai= Atom()
+        si= structure[ia]
+        crd= si.frac_coords
+        ai.set_pos(crd[0],crd[1],crd[2])
+        sid= structure.symbol_set.index(si.species_string)+1
+        ai.set_sid(sid)
+        ai.set_id(ia+1)
+        aSys.add_atom(ai)
+    return aSys
+
+def write_ergref(fname,energy):
+    f=open(fname,'w')
+    f.write(' {0:20.8f}\n'.format(energy))
+    f.close()
+    
+def write_frcref(fname,forces):
+    f= open(fname,'w')
+    f.write(' {0:10d}\n'.format(len(forces)))
+    for fi in forces:
+        f.write('  {0:12.7f}'.format(fi[0]))
+        f.write('  {0:12.7f}'.format(fi[1]))
+        f.write('  {0:12.7f}'.format(fi[2]))
+        f.write('\n')
+    f.close()
+
+if __name__ == "__main__":
+
+    parser= optparse.OptionParser(usage=_usage)
+    # parser.add_option("-d",dest="dr",type="float",default=0.1, \
+    #                   help="Width of the bin. Default is 0.1.")
+    # parser.add_option("-r",dest="rmax",type="float",default=5.0, \
+    #                   help="Cutoff radius of radial distribution. Default is 5.0.")
+    # parser.add_option("--src-sid",dest="idsrc",type="int",default=0,
+    #                   help="Species-ID of source atoms to be selected.\n" \
+    #                   +"Default is 0. 0 means all the species are selected.")
+    # parser.add_option("--dst-sid",dest="iddst",type="int",default=0,
+    #                   help="Species-ID of source atoms to be selected.\n" \
+    #                   +"Default is 0. 0 means all the species are selected.")
+    # parser.add_option("-p",action="store_true",
+    #                   dest="plot",default=False,
+    #                   help="Plot a graph on the screen.")
+    (options,args)= parser.parse_args()
+
+    #...check arguments
+    if len(args) == 0:
+        pass
+    elif len(args) == 1:
+        _infname = args[0]
+    elif len(args) > 1:
+        print _usage
+        exit
+
+    #...parse vasprun.xml using Vasprun in pymatgen
+    vasprun= Vasprun(_infname)
+    
+    ibrion= vasprun.incar['IBRION']
+
+    if ibrion == 0: # MD
+        nstps= len(vasprun.ionic_steps)
+        for istp in range(nstps):
+            dirname= '{0:05d}'.format(istp+1)
+            os.mkdir(dirname)
+            vaspstep= vasprun.ionic_steps[istp]
+            aSys= structure2aSys(vaspstep['structure'])
+            energy= vaspstep['total']
+            forces= vaspstep['forces']
+            aSys.write_pmd(dirname+'/'+_fname_pos)
+            write_ergref(dirname+'/'+_fname_erg,energy)
+            write_frcref(dirname+'/'+_fname_frc,forces)
+            print ' write ',dirname,_fname_pos,_fname_erg,_fname_frc
+    else: # only final structure is needed
+        aSys= structure2aSys(vasprun.final_structure)
+        energy= vasprun.ionic_steps[-1]['total']
+        forces= vasprun.ionic_steps[-1]['forces']
+        aSys.write_pmd(_fname_pos)
+        write_ergref(_fname_erg,energy)
+        write_frcref(_fname_frc,forces)
+        print ' write ',_fname_pos,_fname_erg,_fname_frc
+    
