30f880d946d0ab51b37187aa81cea13d8461e3c7
Ryo KOBAYASHI <ryo.kbys@gmail.com>
2018-04-09 12:13:19 +0900

    fix a regrex in common.py

--- nappy/common.py
+++ nappy/common.py
@@ -1,6 +1,6 @@
 import re
 
 def get_key(v):
-    prefix, index = re.match(r'([a-z]+)_(\d+)', v).groups()
+    prefix, index = re.match(r'([a-zA-Z]+)_(\d+)', v).groups()
     return prefix, -int(index)
 
