17210c8c3665bb666a8d3f7280cbe2eb757e0e26
Ryo KOBAYASHI <ryo.kbys@gmail.com>
2019-05-15 17:34:21 +0900

    updated version digit.

--- pmd/mod_version.F90
+++ pmd/mod_version.F90
@@ -1,12 +1,12 @@
 module version
 !-----------------------------------------------------------------------
-!                     Last-modified: <2019-01-22 17:00:08 Ryo KOBAYASHI>
+!                     Last-modified: <2019-05-15 17:33:33 Ryo KOBAYASHI>
 !-----------------------------------------------------------------------
 ! A module for version/revision.
 !-----------------------------------------------------------------------
   implicit none
   save
-  character(len=128),parameter:: cversion = 'rev190122'
+  character(len=128),parameter:: cversion = 'rev190515'
 
   character(len=128),parameter:: cauthors(1) = &
        (/ 'Ryo KOBAYASHI <kobayashi.ryo@nitech.ac.jp>' /)
