faa74a6ec39d1240e08040aa616658ea7477534f
KOBAYASHI Ryo <ryo.kbys@gmail.com>
2022-12-04 15:48:51 +0900

    modify some for compatibility to ISSP system B Ohtaka.

--- examples/fp_LATP/subjob.sh
+++ examples/fp_LATP/subjob.sh
@@ -13,6 +13,8 @@ cp ../in.pmd.* ../pmdini ./
 #...cd to the directory and clean up
 rm -f dump_* out.*
 
+t0=`date +%s`
+
 export OMP_NUM_THREADS=2
 
 #...Relax pos and cell
@@ -23,8 +25,7 @@ export OMP_NUM_THREADS=2
 
 #...NpT MD
 cp in.pmd.NpT in.pmd
-#mpirun -np 1 pmd 2>&1 > out.pmd.NpT
-#srun -n 1 -c 1 ../../../pmd/pmd 2>&1 > out.pmd.NpT
+#../../../pmd/pmd 2>&1 > out.pmd.NpT
 mpirun -np 1 ../../../pmd/pmd 2>&1 > out.pmd.NpT
 head -n166 out.pmd.NpT
 tail -n20 out.pmd.NpT
@@ -33,4 +34,6 @@ echo "NpT-MD done at" `date`
 python ../../../nappy/rdf.py -d 0.05 -r 5.0 --gsmear=2 --skip=80 --specorder=Li,Al,Ti,P,O --pairs Li-O,Al-O,Ti-O,P-O,O-O --out4fp -o data.pmd.rdf pmd_[0-9]* 2>&1 
 python ../../../nappy/adf.py --gsmear=2 --triplets=Al-O-O,Ti-O-O,P-O-O --skip=80 --out4fp -o data.pmd.adf pmd_[0-9]* 2>&1 
 python ../../../nappy/vol_lat.py --skip=80 --out4fp --prefix data.pmd pmd_[0-9]* 2>&1
-echo "post-processing done at" `date`
+t1=`date +%s`
+etime=`expr $t1 - $t0`
+echo "post-processing took" $etime "sec, done at" `date`
