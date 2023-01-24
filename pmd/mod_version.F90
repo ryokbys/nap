80824e845b1e64f5e098ef4f681f1be357d69c12
KOBAYASHI Ryo <ryo.kbys@gmail.com>
2023-01-24 15:24:36 +0900

    change version number.

--- pmd/mod_version.F90
+++ pmd/mod_version.F90
@@ -1,6 +1,6 @@
 module version
 !-----------------------------------------------------------------------
-!                     Last-modified: <2023-01-18 14:20:57 KOBAYASHI Ryo>
+!                     Last-modified: <2023-01-24 15:24:25 KOBAYASHI Ryo>
 !-----------------------------------------------------------------------
 ! A module for version/revision.
 !-----------------------------------------------------------------------
@@ -10,7 +10,7 @@ module version
 
   public:: write_revision, write_authors
   
-  character(len=128),parameter:: crevision = 'rev230118'
+  character(len=128),parameter:: crevision = 'rev230124'
 
   character(len=128),parameter:: cauthors(1) = &
        (/ 'Ryo KOBAYASHI <kobayashi.ryo@nitech.ac.jp>' /)
